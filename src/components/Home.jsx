786bc834e7e68c68a0920c5dc7acc7f2fede5953
Tynneth Jhoel <tj.fuentes15@gmail.com>
2024-02-09 16:37:55 +0800

    rename title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,14 +11,18 @@ const Home = (props) => {
           <Button name="Sign Up" style="bg-primary px-3 py-2 rounded" />
         </div>
       </header>
-      <section className="p-3">
-        <h1 className="text-primary font-extrabold text-4xl p-2">CornFlix</h1>
-        <p className="text-lg text-justify px-2">
+      <section className="p-3 space-y-5">
+        <h1 className="text-primary font-bold text-5xl p-2">REVIEW</h1>
+        <p className="text-xs text-justify px-2 opacity-80">
           Discover the magic of cinema. Explore a diverse movie catalog,
           featuring the latest releases and timeless classics. From cast details
           to plot summaries, find everything you need for an immersive movie
           experience.
         </p>
+        <Button
+          name="Create Account"
+          style="mx-2 px-4 py-1 bg-primary rounded font-medium"
+        />
       </section>
     </article>
   );
